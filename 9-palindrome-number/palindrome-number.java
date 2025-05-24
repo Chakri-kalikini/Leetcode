768fdac882ffd3c22d84d57db29d7d9b2f52aad1
kalikini Chakradhar <kalikinichakradhar@gmail.com>
2025-05-24 10:54:42 +0530

    Time: 4 ms (100.00%) | Memory: 43.9 MB (76.35%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false; 
+        }
+
+        int originalNumber = x;
+        int reversedNumber = 0;
+
+        while (x > 0) {
+            int lastDigit = x % 10; 
+            reversedNumber = (reversedNumber * 10) + lastDigit; 
+            x /= 10; 
+        }
+
+        return originalNumber == reversedNumber;
+    }
+}
